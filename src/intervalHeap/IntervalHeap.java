fa5b641dcaf5eb36270f2220cc7af1560aa20c5f
Rahul Wahi <rahulwahi42@gmail.com>
2019-12-21 16:43:47 -0500

    Bug fixes

--- src/intervalHeap/IntervalHeap.java
+++ src/intervalHeap/IntervalHeap.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class IntervalHeap<T> {
 
 	int size ; 
-	ArrayList<Node> ihArray ;
+	ArrayList<Node<T>> ihArray ;
 	Comparator<T> comp ;
 	
 	public IntervalHeap( Comparator<T> comp )
@@ -27,14 +27,14 @@ public class IntervalHeap<T> {
 	{
 		if(size == 0)
 		{
-			Node newnNode = new Node() ;
+			Node<T> newnNode = new Node<>() ;
 			newnNode.left = element ;
 			ihArray.add(newnNode) ;
 			size++ ;
 		}
 		else
 		{
-			Node node = ihArray.get(size-1) ;
+			Node<T> node = ihArray.get(size-1) ;
 			
 			if(node.right == null)
 			{
@@ -55,11 +55,11 @@ public class IntervalHeap<T> {
 			}
 			else
 			{
-				Node newnNode = new Node() ;
+				Node<T> newnNode = new Node<>() ;
 				newnNode.left = element ;
 				ihArray.add(newnNode) ;
 				minBottomUpHeapify(size) ; // if smaller this will run
-				maxBottomUpHeapify(size - 1) ;// if larger this will run
+				maxBottomUpHeapify(size) ;// if larger this will run
 				size++;
 			}
 		}
@@ -73,7 +73,7 @@ public class IntervalHeap<T> {
 	Description: This function will start the min heapification from the given index and will continue up till the root 
 	Return: none
 	 ***********************/
-	public void minBottomUpHeapify(int index)
+	private void minBottomUpHeapify(int index)
 	{
 		while( parent(index) != index && comp.compare((T)ihArray.get( parent(index) ).left , (T)ihArray.get( index ).left ) > 0   )
 		{
@@ -92,11 +92,19 @@ public class IntervalHeap<T> {
 				 if necessary swap left and right elements of node
 	Return: none
 	 ***********************/
-	public void minTopdownHeapify(int index)
+	private void minTopdownHeapify(int index)
 	{
 		int smallestIndex = index ;
 		int leftIndex  = left(index) ;
 		int rightIndex = right(index) ;
+		
+		T tmp1 =  (T) ihArray.get( index ).left ;
+		//Swap right and left if left is greater than right
+		if( comp.compare( tmp1 , (T) ihArray.get( index ).right  ) > 0)
+		{
+				ihArray.get( index ).left = ihArray.get( index ).right ;
+				ihArray.get( index ).right = tmp1 ;
+		}
 		
 		if( leftIndex < size  && comp.compare((T)ihArray.get( leftIndex ).left , (T)ihArray.get( smallestIndex ).left ) < 0 )
 		{
@@ -128,23 +136,121 @@ public class IntervalHeap<T> {
 	}
 	
 	/***********************
+	Function Name: maxTopdownHeapify
+	Argument: int index
+	Description: This function will start the max heapification from the given index and will continue down till the leaf elements 
+				 if necessary swap left and right elements of node
+	Return: none
+	 ***********************/
+	private void maxTopdownHeapify(int index)
+	{
+		int biggestIndex = index ;
+		int leftIndex  = left(index) ;
+		int rightIndex = right(index) ;
+		
+		if( leftIndex < size  &&  (T)ihArray.get( leftIndex ).right == null  )
+		{
+			
+			if(comp.compare((T)ihArray.get( leftIndex ).left , (T)ihArray.get( biggestIndex ).right ) > 0)
+			{
+				//if greater left child left is greater than parent right
+				T tmp = (T)ihArray.get( leftIndex ).left ;
+				ihArray.get( leftIndex ).left  = ihArray.get( biggestIndex ).right ;
+				ihArray.get( biggestIndex ).right = tmp ;
+				return ;
+				
+			}
+			return ;
+		}
+		
+		if( leftIndex < size  && comp.compare((T)ihArray.get( leftIndex ).right , (T)ihArray.get( biggestIndex ).right ) > 0 )
+		{
+			biggestIndex = leftIndex ;
+		}
+		
+		if( rightIndex < size  && ihArray.get( rightIndex ).right == null )
+		{
+			//if biggestIndex is same as index , than change the 
+			if( biggestIndex == index )
+			{
+				if(comp.compare((T)ihArray.get( rightIndex ).left , (T)ihArray.get( biggestIndex ).right ) > 0)
+				{
+					//if greater left child left is greater than parent right
+					T tmp = (T)ihArray.get( rightIndex ).left ;
+					ihArray.get( rightIndex ).left  = ihArray.get( biggestIndex ).right ;
+					ihArray.get( biggestIndex ).right = tmp ;
+					return ;
+					
+				}
+				return ;
+			}
+		}
+		
+		if( rightIndex < size  && comp.compare((T)ihArray.get( rightIndex ).right , (T)ihArray.get( biggestIndex ).right ) > 0 )
+		{
+			biggestIndex = rightIndex ;
+		}
+		
+		if( biggestIndex != index)
+		{
+			T tmp =  (T) ihArray.get( index ).right ;
+			ihArray.get( index ).right = ihArray.get( biggestIndex ).right ;
+			//Swap right and left if left is greater than right
+			if( comp.compare( tmp , (T) ihArray.get( biggestIndex ).left  ) > 0)
+			{
+					ihArray.get( biggestIndex ).right = ihArray.get( biggestIndex ).left ;
+					ihArray.get( biggestIndex ).left = tmp ;
+					
+			}
+			else
+			{
+				ihArray.get( biggestIndex ).right = tmp ;
+			}
+			
+			maxTopdownHeapify(biggestIndex) ;
+		}
+	}
+	
+	
+	/***********************
 	Function Name: maxBottomupHeapify
 	Argument: int index
 	Description: This function will start the max heapification from the given index and will continue up till the root 
 	Return: none
 	 ***********************/
-	public void maxBottomUpHeapify(int index)
+	private void maxBottomUpHeapify(int index)
 	{
-		while( parent(index) != index && comp.compare((T)ihArray.get( parent(index) ).left , (T)ihArray.get( index ).left ) < 0   )
+		T element = (T)ihArray.get( index ).right ;
+		//case when last inserted node has only has left end point
+		if( element == null )
 		{
-			T tmp = (T) ihArray.get( index ).left ;
-			ihArray.get( index ).left = ihArray.get( parent(index) ).left ;
-			ihArray.get( parent(index) ).left = tmp ;
-			
+			element = (T)ihArray.get( index ).left ;
+		}
+		while( parent(index) != index && comp.compare((T)ihArray.get( parent(index) ).right , element ) < 0   )
+		{
+//			T tmp = (T) ihArray.get( index ).right ;
+//			ihArray.get( index ).right = ihArray.get( parent(index) ).right ;
+//			
+//			
+			if( ihArray.get( index ).right != null )
+			{
+				ihArray.get( index ).right = ihArray.get( parent(index) ).right  ;
+			}
+			else
+			{
+				ihArray.get( index ).left = ihArray.get( parent(index) ).right  ;
+			}
+			ihArray.get( parent(index) ).right = element ;
 			index = parent(index) ;
 		}
 	}
 	
+	/***********************
+	Function Name: removeMin
+	Argument: None
+	Description: This function will remove the minimum(root element's left) value and return it and heapify for correcting the heap
+	Return: type T
+	 ***********************/
 	public T removeMin()
 	{
 		T min ;
@@ -187,6 +293,7 @@ public class IntervalHeap<T> {
 		else
 		{
 			ihArray.get(size-1).left = ihArray.get(size-1).right ;
+			ihArray.get(size-1).right = null ;
 		}
 		
 		//Call top Down min heapification
@@ -196,6 +303,12 @@ public class IntervalHeap<T> {
 		
 	}
 	
+	/***********************
+	Function Name: removeMax
+	Argument: None
+	Description: This function will remove the max(root element's right, if right null than root's right) value and return it and heapify for correcting the heap
+	Return: type T
+	 ***********************/
 	public T removeMax()
 	{
 		T max ;
@@ -225,7 +338,7 @@ public class IntervalHeap<T> {
 		max = (T) ihArray.get(0).right ; 
 		
 		//Replace with last node left point
-		ihArray.get(0).left = ihArray.get(size-1).left ;
+		ihArray.get(0).right = ihArray.get(size-1).right ;
 		
 		//If last node has no right feild than delete the node , else replace the left with right node value
 		// and set right to null
@@ -243,10 +356,10 @@ public class IntervalHeap<T> {
 			ihArray.get(size-1).right = null ;
 		}
 		
-		//Call top Down min heapification
+		//Call top Down max heapification
 		maxTopdownHeapify(0) ;		
 				
-		return min ;
+		return max ;
 		
 	}
 	
@@ -280,13 +393,23 @@ public class IntervalHeap<T> {
 		return (T) ihArray.get(0).right ;
 	}
 	
+	public void print()
+	{
+		System.out.println( ihArray.toString() );
+	}
+	
+	public boolean isEmpty()
+	{
+		return size == 0 ;
+	}
+	
 	/***********************
 	Function Name: parent
 	Argument: int index
 	Description: This function will return parent's index of the given index
 	Return: int
 	 ***********************/
-	public int parent( int i )
+	private int parent( int i )
 	{
 		return (i-1)/2 ;
 	}
@@ -297,7 +420,7 @@ public class IntervalHeap<T> {
 	Description: This function will return left child's index of the given index
 	Return: int
 	 ***********************/
-	public int left( int i )
+	private int left( int i )
 	{
 		return 2*i + 1 ;
 	}
@@ -308,7 +431,7 @@ public class IntervalHeap<T> {
 	Description: This function will return right child's index of the given index
 	Return: int
 	 ***********************/
-	public int right( int i )
+	private int right( int i )
 	{
 		return 2*i + 2 ;
 	}
@@ -316,6 +439,12 @@ public class IntervalHeap<T> {
 	class Node<T>
 	{
 		public T left, right ;
+		
+		@Override
+		public String toString() 
+	    { 
+	        return  "( " + left + " , " + right + " ) " ; 
+	    } 
 	}
 	
 }
--- src/intervalHeap/TestClass.java
+++ src/intervalHeap/TestClass.java
@@ -4,14 +4,33 @@ import java.util.Comparator;
 
 public class TestClass {
 
-	IntervalHeap<Integer> iheap = new IntervalHeap( (Comparator) new Comparator<Integer>() {
+	public static void main(String[] args)
+	{
+		IntervalHeap<Integer> iheap = new IntervalHeap<Integer> ( (Comparator) new Comparator<Integer>() {
+			
+			@Override
+			public int compare(Integer a, Integer b)
+			{
+				return Integer.compare(a, b) ;
+			}
+		}) ;
 		
-		@Override
-		public int compare(Integer a, Integer b)
-		{
-			return Integer.compare(a, b) ;
-		}
-	}) ;
+		iheap.insert(1) ;
+		iheap.print();
+		iheap.insert(2) ;
+		iheap.print();
+		iheap.insert(3) ;
+		iheap.print();
+		iheap.insert(4) ;
+		iheap.print();
+		iheap.insert(5) ;
+		iheap.print();
+		iheap.insert(6) ;
+		iheap.print();
+		iheap.removeMin() ;
+		iheap.print();
+		iheap.removeMax() ;
+		iheap.print();
+	}
 	
-	iheap.insert(1) ;
 }
